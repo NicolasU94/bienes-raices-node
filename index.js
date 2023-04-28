d30d4bf0ffda1f99c7a9d9d37572cdc1e3e25d8d
Nicolas Ugarte <73134407+NicolasU94@users.noreply.github.com>
2023-04-28 19:29:58 -0300

    Several changes included
    Changes include:
    
    Confirm account screen and email
    Link to confirm account
    Token generated and user confirmed on Db
    CSRF Protection to forms
    Account successfully created screen

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //const express = require("express"); // Common JS way
 import express from "express";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import db from "./config/db.js";
+
 //Creating the app
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
+//Enabling cookie parser
+app.use(cookieParser());
+
+//Enabling csurf
+app.use(csrf({ cookie: true }));
 
 //* Db Connection is being openned //
 try {
